945384ffec242e85855a2ffb451e5e107c4cbf11
Brice <brice.bitot@linkt.fr>
2019-11-20 16:03:21 +0100

    Creation de partie

--- ApiJdr/Controllers/PartieController.cs
+++ ApiJdr/Controllers/PartieController.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using ApiJdr.Models;
+using System;
 using System.Web.Http;
 
 namespace ApiJdr.Controllers
 {
     public class PartieController : ApiController
     {
+        private jdrEntities db = new jdrEntities();
+        public bool Ajouter(string titre, string description)
+        {
+            try
+            {
+                partie newPartie = new partie
+                {
+                    DESCRIPTION_PARTIE = description,
+                    TITRE = titre
+                };
+                db.partie.Add(newPartie);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
