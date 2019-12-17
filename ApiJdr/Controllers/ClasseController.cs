708a8790f85149b9f47c3cd8b4097b0ed04c11a3
Brice <brice.bitot@linkt.fr>
2019-12-17 14:32:35 +0100

    Je force valentin a faire des trucs

---
+++ ApiJdr/Controllers/ClasseController.cs
@@ -0,0 +1,19 @@
+﻿using ApiJdr.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiJdr.Controllers
+{
+    public class ClasseController : ApiController
+    {
+        private jdrEntities db = new jdrEntities();
+
+        [HttpGet]
+        public List<classe> GetAll()
+        {
+            return db.classe.ToList();
+        }
+
+    }
+}
